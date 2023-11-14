d958c24e9025f4163cf904ab16751d68b3546833
The-Joker <henos.misganaw@gmail.com>
2023-11-14 18:14:20 +0300

    Finish working the whole Html, Css and JS of Etch-a-Sketch project

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 let color = "black";
+let click = false;
 
 
 
 document.addEventListener("DOMContentLoaded", function(){
    createBoard(16);
+
+   document.querySelector("body").addEventListener("click", function(e){
+     if(e.target.tagName != "BUTTON"){
+        click = !click;
+        let draw = document.querySelector("#draw");
+        if(click){
+            draw.innerHTML = "Ready To Draw!"; 
+        }
+        else{
+            draw.innerHTML = "Not Allowed To Draw";
+        }
+   }
+})
 
    let btn_popup = document.querySelector("#popup");
    btn_popup.addEventListener("click", function(){
@@ -44,12 +58,14 @@ function getSize(){
 }
 
 function colorDiv(){
+  if(click){
     if(color == "random"){
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     }
     else{
         this.style.backgroundColor = "black";
     }
+}
 }
 
 function setColor(colorChoice){
